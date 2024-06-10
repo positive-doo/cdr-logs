c107212a7fc1d8f90579a7f0263ccf6fe909c4d7
Nemanja Perunicic <nemanja.perun98@gmail.com>
2024-06-10 19:12:24 +0200

    Initial commit

---
+++ list_clients.py
@@ -0,0 +1,22 @@
+import os
+import requests
+import streamlit as st
+
+
+API_BASE_URL = os.getenv("TRMM_BASE_URL")
+HEADERS = {
+    "Content-Type": "application/json",
+    "X-API-KEY": os.getenv("TRMM_NP"),
+    }
+
+def list_clients():
+    """Fetch the list of clients."""
+    response = requests.get(f'{API_BASE_URL}/clients/', headers=HEADERS)
+    if response.status_code == 200:
+        clients = response.json()
+        for client in clients:
+            print(f'Client ID: {client["id"]}, Client Name: {client["name"]}')
+    else:
+        print(f'Failed to fetch clients: {response.status_code}')
+
+list_clients()
