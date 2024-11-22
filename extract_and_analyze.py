28fe6cca351112a992aebb919a234250114b23e1
Nemanja Peruničić <nemanja.perun98@gmail.com>
2024-11-22 12:47:26 +0100

    Update extract_and_analyze.py

--- extract_and_analyze.py
+++ extract_and_analyze.py
@@ -4,7 +4,7 @@ from pages import page1, page2
 st.set_page_config(layout="wide")
 
 def main():
-    st.sidebar.title("Navigation")
+    st.sidebar.title("Navigation; test staging")
     page = st.sidebar.selectbox("Go to", ["Fetch clients & their workstations", "Page2"], index=None)
 
     if page == "Fetch clients & their workstations":
